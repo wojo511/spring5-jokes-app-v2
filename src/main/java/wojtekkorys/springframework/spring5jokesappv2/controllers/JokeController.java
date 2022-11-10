284c66def1d6ddaaa3af7ee55c2b42597f2d0a9d
wojtek <wojtek.korys@wp.pl>
2022-11-10 22:22:08 +0100

    JokeController implementation

---
+++ src/main/java/wojtekkorys/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package wojtekkorys.springframework.spring5jokesappv2.controllers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import wojtekkorys.springframework.spring5jokesappv2.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke",jokeService.getJoke());
+
+        return "index";
+    }
+}
